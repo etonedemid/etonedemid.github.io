c0aea3c8f438bca7a089ae2abaac3d0a45e6e4d1
etonedemid <3damir0n3@gmail.com>
2024-10-16 22:04:32 -0300

    aa

--- js/login.js
+++ js/login.js
@@ -14,19 +14,30 @@ function login()
 }
 function loginsuccess() 
 {
-    window.location.href = "../database.html";
+    document.querySelector("#login").style.display = "none";
+    document.querySelector("#grid").style.display = "block";
+    setTimeout(redirect, 5000);
 }
 
+function redirect() 
+{
+    window.location.href = "../database.html";
+}
 function accessdenied() 
 {
     document.querySelector("#accessdenied").style.display = "block"; 
+    document.querySelector("#logininput").value = "";
+    document.querySelector("#passwordinput").value = "";
     document.body.style.backgroundColor = "red";
     setTimeout(() => {
         document.body.style.backgroundColor = "";
-    }, 100);
+    }, 300);
 
 }
 function exit() {
     window.close();
 }
 
+function toprofile() {
+    window.location.href = "../profile.html";
+}
